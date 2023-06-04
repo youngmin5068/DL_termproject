aeaf1b48397c61bf1123996530538dcfdf9f41d3
keemym <youngmin980118@gmail.com>
2023-06-05 07:44:04 +0900

    ensemble

--- ensemble.py
+++ ensemble.py
@@ -12,18 +12,12 @@ lr = 0.01
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-# transform_test = transforms.Compose([
-#         transforms.ToTensor(),
-#         # transforms.Resize(256),
-#         # transforms.RandomCrop(224),
-#         transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
-#     ])
 
 # Define the data transforms
 transform_test = transforms.Compose([
     transforms.Resize(256),
     transforms.TenCrop(224),
-    transforms.Lambda(lambda crops: torch.stack([transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])(transforms.ToTensor()(crop)) for crop in crops])),
+    transforms.Lambda(lambda crops: torch.stack([transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])(transforms.ToTensor()(crop)) for crop in crops])),
 ])
 # Load the CIFAR-10 test dataset
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
@@ -35,7 +29,7 @@ for i in range(model_num):
     # Define the ResNet-18 model with pre-trained weights
     model = timm.create_model('resnet18', num_classes=10)
     model = nn.DataParallel(model,device_ids=[0,1,2,3]) 
-    model.load_state_dict(torch.load(f"/workspace/resnet/resnet18_cifar10_%f_%d.pth" % (lr, i)))  # Load the trained weights
+    model.load_state_dict(torch.load(f"/workspace/resnet/new_resnet18_cifar10_%f_%d.pth" % (lr, i)))  # Load the trained weights
     model.eval()  # Set the model to evaluation mode
     model = model.to(device)  # Move the model to the GPU
     models.append(model)
@@ -53,7 +47,6 @@ with torch.no_grad():
         for model in models:
             model_output = model(images.view(-1, c, h, w))  # Reshape the input to (bs*10, c, h, w)
             model_output = model_output.view(bs, ncrops, -1).mean(1)  # Average the predictions of the 10 crops
-            model_output = model(images)
             outputs += model_output
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
